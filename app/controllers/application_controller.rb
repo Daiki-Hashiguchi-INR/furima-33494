103ebf5d53d470cf668b7603cf6742fe236125a0
hassi-INR <hassi.d.cat@gmail.com>
2021-01-14 19:19:15 +0900

    コントローラーの一部修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :encrypted_password, :last_name, :first_name, :last_kana_name,
+                                      keys: [:nickname, :last_name, :first_name, :last_kana_name,
                                              :first_kana_name, :birthday])
   end
 end
