caea26248f1859080c7e495e68315326bf766300
hassi-INR <hassi.d.cat@gmail.com>
2021-01-15 17:51:15 +0900

    ActiveHash作成及び編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,23 @@
 class Item < ApplicationRecord
-  belonges_to :user
+  belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :post_type
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :preparation_day
+
+  with_options presence: true do
+    validates :name
+    validates :explain
+    validates :price, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }
+  end
+
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :condition_id, numericality: { other_than: 1 } 
+  validates :post_type_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :preparation_day_id, numericality: { other_than: 1 } 
 end
