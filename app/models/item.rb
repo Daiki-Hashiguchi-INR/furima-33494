771cbcb32081f5931f2076c5014374e2bb7453f5
hassi-INR <hassi.d.cat@gmail.com>
2021-01-15 19:09:04 +0900

    新規ビューの作成及びモデルの編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,14 +10,19 @@ class Item < ApplicationRecord
   belongs_to_active_hash :preparation_day
 
   with_options presence: true do
+    validates :image
     validates :name
     validates :explain
-    validates :price, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }
   end
 
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :condition_id, numericality: { other_than: 1 } 
-  validates :post_type_id, numericality: { other_than: 1 } 
-  validates :prefecture_id, numericality: { other_than: 1 } 
-  validates :preparation_day_id, numericality: { other_than: 1 } 
+  validates :price, numericality: { only_interger: true, message: 'Half-width number' }
+  validates :price, numericality: { greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999 , message: 'Out of setting range' }
+
+  with_options numericality: { other_than: 1, message: 'Select' } do
+  validates :category_id 
+  validates :condition_id 
+  validates :post_type_id 
+  validates :prefecture_id
+  validates :preparation_day_id 
+  end
 end
