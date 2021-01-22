2b731bb0620f0217479f301a7b3764dbe1632f54
hassi-INR <hassi.d.cat@gmail.com>
2021-01-22 15:39:14 +0900

    Pay.jpの導入、及び編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,19 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
+    if @item.user.id == current_user.id || @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def create
     @order_address = OrderAddress.new(order_params)
     @item = Item.find(params[:item_id])
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -18,6 +24,16 @@ class OrdersController < ApplicationController
   private
   def order_params
     params.require(:order_address).permit(:post_number, :city, :address, :phone_number, :building, :prefecture_id, 
-      :hoge).merge(user_id: current_user.id, item_id: params[:item_id])
+      :number, :exp_month, :exp_year, :cvc ).merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
   end
+  
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item[:price], 
+      card: order_params[:token], 
+      currency: 'jpy'
+    )
+  end
+
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,13 +1,15 @@
 class OrderAddress
   include ActiveModel::Model
   attr_accessor :post_number, :city, :address, 
-                :phone_number, :building, :prefecture_id, :order_id, :item_id, :user_id, :hoge
+                :phone_number, :building, :prefecture_id, :order_id, :item_id, :user_id,
+                :number, :exp_month, :exp_year, :cvc, :token 
 
   with_options presence: true do
     validates :post_number, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'Input correctly' }
     validates :city
     validates :address
     validates :phone_number, numericality: { only_interger: true, message: 'Half-width number' }, length: { maximum: 11, message: 'Over number'}
+    validates :token, presence: true
   end
 
   validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
