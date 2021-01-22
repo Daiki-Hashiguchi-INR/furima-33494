412d89b9a401d43e4eb83ab2d3f68a0262c638a9
hassi-INR <hassi.d.cat@gmail.com>
2021-01-22 18:06:52 +0900

    Rubocopによる修正

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :order_address do
     token { 'tok_abcdefghijk00000000000000000' }
 
-    user_id {2}
-    item_id {2}
+    user_id { 2 }
+    item_id { 2 }
 
     post_number { '222-2222' }
     prefecture_id { 2 }
